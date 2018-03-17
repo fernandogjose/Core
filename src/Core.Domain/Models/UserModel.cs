35df9b6ceaa8a6c84154df7d1350cb9cb8009ffb
Fernando Gonçalves José <fernandogjose@gmail.com>
2018-03-16 22:39:40 -0300

    gitIgnore

--- src/Core.Domain/Models/UserModel.cs
+++ src/Core.Domain/Models/UserModel.cs
@@ -10,6 +10,8 @@ namespace Core.Domain.Models
 
         public string Password { get; private set; }
 
+        public string Name { get; set; }
+
         public UserModel(int id, string email, string password)
         {
             ValidateValues(email, password);
